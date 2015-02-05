011fa0c3b8edfec541e32b14e0f37a22e2e458ed
Vinod Gupta <vinod_gupta@live.com>
2015-02-04 21:15:08 -0800

    basic string search works

--- StringSearch/main.cpp
+++ StringSearch/main.cpp
@@ -8,8 +8,35 @@
 
 #include <iostream>
 
+const char *StringSearch(const char *text,const char *pattern)
+{
+    int i=0;
+    while(text[i] != '\0')
+    {
+        int j=0;
+        while(text[i]==pattern[j])
+        {
+            if(pattern[j+1] == '\0')
+                return &text[i-j];
+            
+            ++j;
+            ++i;
+        }
+        
+        if(j)//This could be eliminated in KMP algo
+            i-=j;
+
+        ++i;
+    }
+    return NULL;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    char text[] = "This is Vinod Gupta herhere here...";
+    char pattern[] = "here";
+    
+    std::cout << strstr(text,pattern)<<std::endl;
+    std::cout << StringSearch(text,pattern)<<std::endl;
     return 0;
 }
